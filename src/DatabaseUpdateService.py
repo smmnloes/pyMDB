04f3bb4f0b8dd5872eafc92c63905560ded614af
Maximilian Lösch <m.loesch@hotmail.de>
2018-03-15 23:55:18 +0100

    finish download and unzip logic. add backup restore funtion

--- src/DatabaseUpdateService.py
+++ src/DatabaseUpdateService.py
@@ -1,8 +1,9 @@
+import gzip
+import os
 import sqlite3
 import urllib.request
-from pathlib import Path
 
-PATH_DB_LAST_VERSION = "../DB_Data_Local/last_version"
+PATH_LOCAL_DB_LAST_VERSION = "../DB_Data_Local/last_version/local.db"
 ROOT_DB_DATA_REMOTE = "../DB_Data_Remote/"
 PATH_LOCAL_DB = "../DB_Data_Local/local.db"
 
@@ -10,18 +11,43 @@ TITLE_BASICS = "basics"
 
 URL_IMDB_DATA_ROOT = "https://datasets.imdbws.com/"
 
+DATASETS_TO_FILENAMES = {'basics': 'title.basics.tsv.gz', 'names': 'name.basics.tsv.gz', 'akas': 'title.akas.tsv.gz',
+                         'crew': 'title.crew.tsv.gz', 'principals': 'title.principals.tsv.gz',
+                         'ratings': 'title.ratings.tsv.gz'}
+
 
 class DatabaseUpdateService:
     def update_db(self):
         self.backup_last_version()
 
-    def backup_last_version(self):
-        current_db_file = Path(PATH_LOCAL_DB)
-        if current_db_file.is_file():
-            current_db_file.rename(Path(PATH_DB_LAST_VERSION, current_db_file.name))
+        try:
+            for dataset in DATASETS_TO_FILENAMES:
+                self.download_new_data(dataset)
+        except Exception:
+            self.restore_last_version()
 
-    def download_new_data(self):
-        urllib.request.urlretrieve(URL_IMDB_DATA_ROOT + 'title.basics.tsv.gz', ROOT_DB_DATA_REMOTE + 'basics_zipped')
+    def backup_last_version(self):
+        if os.path.isfile(PATH_LOCAL_DB):
+            os.rename(PATH_LOCAL_DB, PATH_LOCAL_DB_LAST_VERSION)
+
+    def restore_last_version(self):
+        if os.path.isfile(PATH_LOCAL_DB_LAST_VERSION):
+            os.rename(PATH_LOCAL_DB_LAST_VERSION, PATH_LOCAL_DB)
+        else:
+            print("No previous version found! Sorry...")
+
+    def download_new_data(self, dataset):
+        unzipped_path = ROOT_DB_DATA_REMOTE + dataset
+        zipped_path = unzipped_path + '_zipped'
+        urllib.request.urlretrieve(URL_IMDB_DATA_ROOT + DATASETS_TO_FILENAMES.get(dataset),
+                                   zipped_path)
+
+        with gzip.open(zipped_path) as zipped_file:
+            with open(unzipped_path, 'wb') as unzipped_file:
+                unzipped_file.write(zipped_file.read())
+
+        os.remove(zipped_path)
+
 
     def read_title_basisc(self):
         conn = sqlite3.connect(PATH_LOCAL_DB)
--- src/main.py
+++ src/main.py
@@ -4,8 +4,7 @@ from DatabaseUpdateService import DatabaseUpdateService
 def main():
     print("Welcome to Max' Movie Recommendation Engine!")
     dbupdate = DatabaseUpdateService()
-    #dbupdate.read_title_basisc()
-    dbupdate.download_new_data()
+    dbupdate.update_db()
 
 
 main()
