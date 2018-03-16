9f2e6d80974bc91f226cc30c9fe72828ab90abed
Maximilian Lösch <m.loesch@hotmail.de>
2018-03-16 17:00:07 +0100

    add command line input, refactor databaseupdateservice into module

--- src/DatabaseUpdateService.py
+++ src/DatabaseUpdateService.py
@@ -7,8 +7,6 @@ PATH_LOCAL_DB_LAST_VERSION = "../DB_Data_Local/last_version/local.db"
 ROOT_DB_DATA_REMOTE = "../DB_Data_Remote/"
 PATH_LOCAL_DB = "../DB_Data_Local/local.db"
 
-TITLE_BASICS = "basics"
-
 URL_IMDB_DATA_ROOT = "https://datasets.imdbws.com/"
 
 DATASETS_TO_FILENAMES = {'basics': 'title.basics.tsv.gz', 'names': 'name.basics.tsv.gz', 'akas': 'title.akas.tsv.gz',
@@ -16,62 +14,68 @@ DATASETS_TO_FILENAMES = {'basics': 'title.basics.tsv.gz', 'names': 'name.basics.
                          'ratings': 'title.ratings.tsv.gz'}
 
 
-class DatabaseUpdateService:
-    def update_db(self):
-        self.backup_last_version()
+def update_db():
+    backup_last_version()
 
-        try:
-            for dataset in DATASETS_TO_FILENAMES:
-                self.download_new_data(dataset)
-        except Exception:
-            self.restore_last_version()
-
-    def backup_last_version(self):
-        if os.path.isfile(PATH_LOCAL_DB):
-            os.rename(PATH_LOCAL_DB, PATH_LOCAL_DB_LAST_VERSION)
-
-    def restore_last_version(self):
-        if os.path.isfile(PATH_LOCAL_DB_LAST_VERSION):
-            os.rename(PATH_LOCAL_DB_LAST_VERSION, PATH_LOCAL_DB)
-        else:
-            print("No previous version found! Sorry...")
-
-    def download_new_data(self, dataset):
-        unzipped_path = ROOT_DB_DATA_REMOTE + dataset
-        zipped_path = unzipped_path + '_zipped'
-        urllib.request.urlretrieve(URL_IMDB_DATA_ROOT + DATASETS_TO_FILENAMES.get(dataset),
-                                   zipped_path)
-
-        with gzip.open(zipped_path) as zipped_file:
-            with open(unzipped_path, 'wb') as unzipped_file:
-                unzipped_file.write(zipped_file.read())
-
-        os.remove(zipped_path)
+    try:
+        for dataset in DATASETS_TO_FILENAMES:
+            download_new_data(dataset)
+    except Exception as e:
+        print("Error while updating: {}\nRestoring last dabase version!".format(e))
+        restore_last_version()
 
 
-    def read_title_basisc(self):
-        conn = sqlite3.connect(PATH_LOCAL_DB)
-        c = conn.cursor()
-        c.execute("CREATE TABLE IF NOT EXISTS {}(tconst TEXT PRIMARY KEY , primaryTitle TEXT, originalTitle TEXT, "
-                  "year INTEGER, runtimeMinutes INTEGER, genres TEXT)".format(TITLE_BASICS))
+def backup_last_version():
+    print('Backing up last version!')
+    if os.path.isfile(PATH_LOCAL_DB):
+        os.rename(PATH_LOCAL_DB, PATH_LOCAL_DB_LAST_VERSION)
+    else:
+        print('No database found, nothing to back up.')
 
-        with open(ROOT_DB_DATA_REMOTE + TITLE_BASICS + ".tsv", 'r') as file:
-            line = file.readline()
+
+def restore_last_version():
+    if os.path.isfile(PATH_LOCAL_DB_LAST_VERSION):
+        os.rename(PATH_LOCAL_DB_LAST_VERSION, PATH_LOCAL_DB)
+    else:
+        print("No previous version found! Sorry...")
+
+
+def download_new_data(dataset):
+    unzipped_path = ROOT_DB_DATA_REMOTE + dataset
+    zipped_path = unzipped_path + '_zipped'
+    urllib.request.urlretrieve(URL_IMDB_DATA_ROOT + DATASETS_TO_FILENAMES.get(dataset),
+                               zipped_path)
+
+    with gzip.open(zipped_path) as zipped_file:
+        with open(unzipped_path, 'wb') as unzipped_file:
+            unzipped_file.write(zipped_file.read())
+
+    os.remove(zipped_path)
+
+
+def read_title_basisc():
+    conn = sqlite3.connect(PATH_LOCAL_DB)
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS {}(tconst TEXT PRIMARY KEY , primaryTitle TEXT, originalTitle TEXT, "
+              "year INTEGER, runtimeMinutes INTEGER, genres TEXT)".format('basics'))
+
+    with open(ROOT_DB_DATA_REMOTE + 'basics', 'r') as file:
+        line = file.readline()
+        line = file.readline().strip()
+
+        count = 1
+        while line:
+            entries = line.split('\t')
+
+            if (entries[1] == "movie") & (entries[4] == "0"):
+                del entries[1]
+                del entries[3]
+                del entries[4]
+                c.execute("INSERT OR REPlACE INTO {} VALUES (?,?,?,?,?,?)".format('basics'), entries)
+
             line = file.readline().strip()
+            print("Reading line %d" % count)
+            count += 1
 
-            count = 1
-            while line:
-                entries = line.split('\t')
-
-                if (entries[1] == "movie") & (entries[4] == "0"):
-                    del entries[1]
-                    del entries[3]
-                    del entries[4]
-                    c.execute("INSERT OR REPlACE INTO {} VALUES (?,?,?,?,?,?)".format(TITLE_BASICS), entries)
-
-                line = file.readline().strip()
-                print("Reading line %d" % count)
-                count += 1
-
-        conn.commit()
-        conn.close()
+    conn.commit()
+    conn.close()
--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
-from DatabaseUpdateService import DatabaseUpdateService
+import DatabaseUpdateService
 
 
 def main():
     print("Welcome to Max' Movie Recommendation Engine!")
-    dbupdate = DatabaseUpdateService()
-    dbupdate.update_db()
+    user_input = ''
+    while user_input != 'exit':
+        user_input = input('Please enter Command! ')
 
+        if user_input == 'update':
+            DatabaseUpdateService.update_db()
+        elif user_input == 'download':
+            DatabaseUpdateService.download_new_data('ratings')
+        elif user_input == 'backup':
+            DatabaseUpdateService.backup_last_version()
+        elif user_input == 'restore':
+            DatabaseUpdateService.restore_last_version()
+        elif user_input == 'readbasics':
+            DatabaseUpdateService.read_title_basisc()
 
 main()
