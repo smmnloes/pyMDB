6b06fd7f47b04d57aa50f0ac537676fc198283ca
Maximilian Lösch <m.loesch@hotmail.de>
2018-03-28 19:00:36 +0200

    change gitignore, remove new sqlalchemy functionality temporarily

--- src/DatabaseServices/UpdateService.py
+++ src/DatabaseServices/UpdateService.py
@@ -15,14 +15,14 @@ VALID_IDS = []
 
 
 def update_db():
-    # backup_local_db()
+    backup_local_db()
 
     try:
         for dataset in DATASETS:
             print('\nProcessing %s data.' % dataset)
-            # download_new_data(dataset)
+            download_new_data(dataset)
             DATASETS_TO_READ_FUNCTIONS.get(dataset)()
-            # delete_downloaded_remote_data(dataset)
+            delete_downloaded_remote_data(dataset)
             print('Finished processing %s data.\n' % dataset)
 
     except (Exception, BaseException) as e:
