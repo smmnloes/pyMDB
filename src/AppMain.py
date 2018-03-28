c525d518729f24c6a4ef903f1ba6155a84bc79a1
Maximilian Lösch <m.loesch@hotmail.de>
2018-03-28 14:10:07 +0200

    define primary keys / foreign keys for all tables, implement autoload

--- src/AppMain.py
+++ src/AppMain.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from flask_cors import CORS
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.ext.automap import automap_base
 
 from DatabaseServices import QueryService
 from DatabaseServices.Paths import *
@@ -11,9 +13,18 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + LOCAL_DB
 db = SQLAlchemy(app)
 
-db.reflect(app=app)
+metadata = MetaData()
+metadata.reflect(db.engine)
+Base = automap_base(metadata=metadata)
+Base.prepare()
+Base.query = db.session.query_property()
 
-
+Basics = Base.classes.basics
+query = db.session.query(Basics)
+query = query.add_columns(Basics.primaryTitle, Basics.runtimeMinutes)
+query = query.filter(Basics.primaryTitle == 'Miss Jerry')
+result = query.all()
+print(result)
 
 api = Api(app)
 cors = CORS(app, resources={r"/query": {"origins": "*"}})
