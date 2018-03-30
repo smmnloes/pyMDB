499718ff22d070603c077959b5ce219e1b0c31d3
Maximilian Lösch <m.loesch@hotmail.de>
2018-03-30 15:22:01 +0200

    implement query in sqlalchemy, remove foreign key constraints to make automap less confusing

--- src/DatabaseServices/QueryService.py
+++ src/DatabaseServices/QueryService.py
@@ -1,7 +1,24 @@
-import sqlite3
 import time
 
-from DatabaseServices import Paths
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import aliased
+
+db = None
+basics = None
+names = None
+ratings = None
+
+
+def init_query_service():
+    db.metadata.reflect(db.engine)
+    base = automap_base(metadata=db.metadata)
+    base.prepare()
+    base.query = db.session.query_property()
+
+    global basics, names, ratings
+    basics = base.classes.basics
+    names = base.classes.names
+    ratings = base.classes.ratings
 
 
 def millis():
@@ -9,48 +26,33 @@ def millis():
 
 
 def get_movies_by_criteria(request):
-    db_connect = sqlite3.connect('file:' + Paths.LOCAL_DB + '?mode=ro', uri=True)
-    c = db_connect.cursor()
-
-    query_select = "SELECT b.*,r.averageRating"
-    query_from = "FROM basics b, ratings r"
-    query_from_items = []
-    query_where = " WHERE r.tid=b.tid AND "
-
+    query = db.session.query()
+    query = query.add_columns(ratings.averageRating)
+    names1 = aliased(names)
+    names2 = aliased(names)
     if request['director']:
-        query_where += "n1.name='%s' AND d.nid=n1.nid AND d.tid=b.tid AND " % request['director']
-        query_from_items.append(',directors d,names n1')
+        query = query.filter(basics.names_collection == request['director'])
 
     if request['writer']:
-        query_where += "n2.name='%s' AND w.nid=n2.nid AND w.tid=b.tid AND " % request['writer']
-        query_from_items.append(',writers w,names n2')
+        query = query.filter(basics.writers.name == request['writer'])
 
     if request['year_from']:
-        query_where += "b.year >=%d AND " % request['year_from']
+        query = query.filter(basics.year >= request['year_from'])
 
     if request['year_to']:
-        query_where += "b.year <=%d AND " % request['year_to']
+        query = query.filter(basics.year <= request['year_to'])
 
     if request['minRatingIMDB']:
-        query_where += "r.averageRating>=%.1f AND " % request['minRatingIMDB']
+        query = query.filter(ratings.averageRating >= request['minRatingIMDB'])
 
     if request['genres']:
         for genre in request['genres'].split(','):
-            query_where += "b.genres LIKE '%{}%' AND ".format(genre)
-
-    query_where = query_where.strip(' AND ')
-
-    for item in query_from_items:
-        query_from += item
-
-    query_from = query_from.strip(',')
-
-    query = query_select + " " + query_from + " " + query_where
+            query = query.filter(basics.genres.like("%{}%".format(genre)))
 
     print(query)
     time_before = millis()
-    c.execute(query)
+    results = query.all()
     print("Time taken: " + str(millis() - time_before) + "ms")
-    results = c.fetchall()
+
     print("Results: {}".format(len(results)))
     return results
