8ef318761abf5c80bbd94a2d55f7d3ac0faa372f
Maximilian Lösch <m.loesch@hotmail.de>
2018-04-01 14:35:02 +0200

    queryservice migrated

--- py-backend/src/App/AppMain.py
+++ py-backend/src/App/AppMain.py
@@ -3,21 +3,24 @@ from flask_cors import CORS
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
-from App.RESTControllers import MovieQuery
-from DatabaseServices import QueryService
 from definitions import *
+
+db = SQLAlchemy()
 
 
 def start_app():
-    app = Flask(__name__)
-
-    api = Api(app)
-    api.add_resource(MovieQuery, '/query')
-
-    CORS(app, resources={r"/query": {"origins": "*"}})
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + LOCAL_DB
-    QueryService.db = SQLAlchemy(app)
-    QueryService.init_query_service()
+    app = create_app()
     app.run(port=5002)
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + LOCAL_DB
+    CORS(app, resources={r"/query": {"origins": "*"}})
+    db.init_app(app)
+    api = Api(app)
+
+    from App.RESTControllers import MovieQuery
+    api.add_resource(MovieQuery, '/query')
+    return app
