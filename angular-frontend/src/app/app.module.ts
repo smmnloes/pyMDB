d524a6f3f91df33b8a7ec63c47fd1ea8a8a3ccba
maximilian <m.loesch@hotmail.de>
2018-04-04 15:45:13 +0200

    create header,content,search-page components

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -3,11 +3,21 @@ import {NgModule} from '@angular/core';
 
 
 import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {ContentComponent} from './content/content.component';
+import {SearchPageComponent} from './content/search-page/search-page.component';
+import {SearchFilterComponent} from './content/search-page/search-filter/search-filter.component';
+import {SearchResultsComponent} from './content/search-page/search-results/search-results.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    SearchPageComponent,
+    SearchFilterComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule
