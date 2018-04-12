8299a8c0aa3cea3adca05c1d9a5d76cdd293b9ad
maximilian <m.loesch@hotmail.de>
2018-04-12 23:20:26 +0200

    implement multiselect dropdown

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import {FormsModule} from "@angular/forms";
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
@@ -8,6 +8,7 @@ import {ContentComponent} from './content/content.component';
 import {SearchPageComponent} from './content/search-page/search-page.component';
 import {SearchFilterComponent} from './content/search-page/search-filter/search-filter.component';
 import {SearchResultsComponent} from './content/search-page/search-results/search-results.component';
+import {AngularMultiSelectModule} from "angular2-multiselect-dropdown/angular2-multiselect-dropdown";
 
 
 @NgModule({
@@ -20,7 +21,9 @@ import {SearchResultsComponent} from './content/search-page/search-results/searc
     SearchResultsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularMultiSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-frontend/src/app/content/search-page/search-filter/search-filter.component.ts
+++ angular-frontend/src/app/content/search-page/search-filter/search-filter.component.ts
@@ -7,9 +7,48 @@ import {Component, OnInit} from '@angular/core';
 })
 export class SearchFilterComponent implements OnInit {
 
-  constructor() { }
+  genreList = [];
+  selectedGenres = [];
+  dropdownSettings = {};
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.genreList = [
+      {"id": 1, "itemName": "India"},
+      {"id": 2, "itemName": "Singapore"},
+      {"id": 3, "itemName": "Australia"},
+      {"id": 4, "itemName": "Canada"},
+      {"id": 5, "itemName": "South Korea"},
+      {"id": 6, "itemName": "Germany"},
+      {"id": 7, "itemName": "France"},
+      {"id": 8, "itemName": "Russia"},
+      {"id": 9, "itemName": "Italy"},
+      {"id": 10, "itemName": "Sweden"}
+    ];
+    this.selectedGenres = [];
+    this.dropdownSettings = {
+      singleSelection: false,
+      text: "Select Genres (3 max)",
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      enableSearchFilter: true,
+      classes: "myclass custom-class"
+    };
+  }
+
+  onItemSelect(item: any) {
+
+    if (this.selectedGenres.length > 3) {
+      this.selectedGenres = [this.selectedGenres[0], this.selectedGenres[1], this.selectedGenres[2]];
+    }
+    console.log(item);
+    console.log(this.selectedGenres);
+  }
+
+  onSelectAll(items: any) {
+    this.selectedGenres = [this.selectedGenres[0], this.selectedGenres[1], this.selectedGenres[2]];
+    console.log(this.selectedGenres);
+  }
 }
