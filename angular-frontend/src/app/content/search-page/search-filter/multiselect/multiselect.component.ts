2602148fe57388ea25564d025c75f761a177eb84
maximilian <m.loesch@hotmail.de>
2018-04-13 17:08:33 +0200

    refactor genre list

--- angular-frontend/src/app/content/search-page/search-filter/multiselect/multiselect.component.ts
+++ angular-frontend/src/app/content/search-page/search-filter/multiselect/multiselect.component.ts
@@ -1,5 +1,6 @@
 import {Component, forwardRef, OnInit} from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+import {imdbGenres} from "./IMDBgenres";
 
 @Component({
   selector: 'app-multiselect',
@@ -40,35 +41,7 @@ export class MultiselectComponent implements OnInit, ControlValueAccessor {
   }
 
   ngOnInit() {
-    this.dropdownList = [{'itemName': 'Horror', 'id': 0},
-      {'itemName': 'Music', 'id': 1},
-      {'itemName': 'Documentary', 'id': 2},
-      {'itemName': 'Film-Noir', 'id': 3},
-      {'itemName': 'News', 'id': 4},
-      {'itemName': 'Comedy', 'id': 5},
-      {'itemName': 'Animation', 'id': 6},
-      {'itemName': 'Sport', 'id': 7},
-      {'itemName': 'Family', 'id': 8},
-      {'itemName': 'Thriller', 'id': 9},
-      {'itemName': 'History', 'id': 10},
-      {'itemName': 'Mystery', 'id': 11},
-      {'itemName': 'Game-Show', 'id': 12},
-      {'itemName': 'War', 'id': 13},
-      {'itemName': 'Romance', 'id': 14},
-      {'itemName': 'Crime', 'id': 15},
-      {'itemName': 'Short', 'id': 16},
-      {'itemName': 'Fantasy', 'id': 17},
-      {'itemName': 'Western', 'id': 18},
-      {'itemName': 'Musical', 'id': 19},
-      {'itemName': 'Sci-Fi', 'id': 20},
-      {'itemName': 'Action', 'id': 21},
-      {'itemName': 'Biography', 'id': 22},
-      {'itemName': 'Adult', 'id': 23},
-      {'itemName': 'Talk-Show', 'id': 24},
-      {'itemName': 'Drama', 'id': 25},
-      {'itemName': 'Adventure', 'id': 26},
-      {'itemName': 'Reality-TV', 'id': 27},
-    ];
+    this.dropdownList = imdbGenres.genres;
 
     this.selectedItems = [];
 
