a78681ba2cb07e348da35c74a4e66abc1d8f8897
maximilian <m.loesch@hotmail.de>
2018-04-13 22:14:55 +0200

    implement queryService

--- angular-frontend/src/app/query.service.ts
+++ angular-frontend/src/app/query.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
 
 
 const httpOptions = {
@@ -8,12 +9,24 @@ const httpOptions = {
 
 @Injectable()
 export class QueryService {
+  result$: Observable<object[]>;
+  observer;
+
 
   constructor(private http: HttpClient) {
+    this.result$ = new Observable((observer) => {
+      this.observer = observer;
+    })
   }
 
+
   makeQuery(queryData) {
-    return this.http.post('api/query', queryData, httpOptions);
+    this.http.post('api/query', queryData, httpOptions).subscribe(
+      data => {
+        this.observer.next(data);
+      }
+    );
   }
+
 
 }
