6773b19e8f52a5c07c0d18e4db277db88b47d730
maximilian <m.loesch@hotmail.de>
2018-04-13 18:41:43 +0200

    REST calls working ,renaming

--- angular-frontend/src/app/content/search-page/search-form/search-form.component.spec.ts
+++ angular-frontend/src/app/content/search-page/search-form/search-form.component.spec.ts
@@ -1,20 +1,20 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import {SearchFilterComponent} from './search-filter.component';
+import {SearchFormComponent} from './search-form.component';
 
-describe('SearchFilterComponent', () => {
-  let component: SearchFilterComponent;
-  let fixture: ComponentFixture<SearchFilterComponent>;
+describe('SearchFormComponent', () => {
+  let component: SearchFormComponent;
+  let fixture: ComponentFixture<SearchFormComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchFilterComponent ]
+      declarations: [ SearchFormComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SearchFilterComponent);
+    fixture = TestBed.createComponent(SearchFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
