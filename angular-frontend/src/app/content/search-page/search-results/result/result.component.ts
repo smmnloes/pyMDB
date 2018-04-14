d6f98c89fcd28015d3f1073d223e4dcc4b90bd55
maximilian <m.loesch@hotmail.de>
2018-04-14 10:30:34 +0200

    result component working

--- angular-frontend/src/app/content/search-page/search-results/result/result.component.ts
+++ angular-frontend/src/app/content/search-page/search-results/result/result.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-result',
@@ -7,7 +7,10 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ResultComponent implements OnInit {
 
-  constructor() { }
+  @Input() movieData: any[];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
