eba6a6c329d06f62971cca598dd6493b01db9401
maximilian <m.loesch@hotmail.de>
2018-04-27 14:56:10 +0200

    posters working

--- angular-frontend/src/app/header/content/details-page/details-page.component.ts
+++ angular-frontend/src/app/header/content/details-page/details-page.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {DetailService} from "../../../detail.service";
+import {HttpClient} from "@angular/common/http";
+import {CombinedDataModel} from "../search-page/search-results/result/combined-data-model";
+import {Subject} from "rxjs/Subject";
 
 @Component({
   selector: 'app-details-page',
@@ -7,20 +10,44 @@ import {DetailService} from "../../../detail.service";
   styleUrls: ['./details-page.component.css']
 })
 export class DetailsPageComponent implements OnInit {
-  private details: Object;
+  private combinedData: CombinedDataModel;
 
-  constructor(private detailService: DetailService) {
+  private fullPosterPathSource = new Subject<String>();
+  private fullPosterPath$ = this.fullPosterPathSource.asObservable();
+
+  constructor(private detailService: DetailService, private http: HttpClient) {
   }
 
   ngOnInit() {
-    this.detailService.currentDetails$.subscribe(details => {
-      if (details == null) {
+    this.detailService.combinedData$.subscribe(combinedData => {
+      if (combinedData == null) {
         console.log("NO DETAILS FOUND!");
       } else {
-        this.details = details;
-        console.log(details);
+        this.combinedData = combinedData;
+        this.getFullPosterPath();
       }
     })
+  }
+
+
+  /*
+  Poster sizes:
+   "w92",
+   "w154",
+   "w185",
+   "w342",
+   "w500",
+   "w780",
+   "original"
+   */
+
+  private getFullPosterPath() {
+    return this.http.get(this.detailService.TMDB_ROOT + 'configuration?api_key=' + this.detailService.TMDB_API_KEY).subscribe(configData => {
+        let baseUrl = configData['images']['base_url'];
+        this.fullPosterPathSource.next(baseUrl + 'w185' + this.combinedData.detailedData.posterPath);
+      }
+    );
+
   }
 
 }
