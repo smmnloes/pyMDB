2b19cf236e9fb560d9df26036c2ac925422c7fbf
maximilian <m.loesch@hotmail.de>
2018-04-27 17:00:58 +0200

    add production countries

--- angular-frontend/src/app/header/content/search-page/search-results/result/detailed-data-model.ts
+++ angular-frontend/src/app/header/content/search-page/search-results/result/detailed-data-model.ts
@@ -3,6 +3,7 @@ export class DetailedDataModel {
   constructor(public credits:string[][],
               public budget:number,
               public originalLanguage:string,
+              public productionCountries:string[],
               public releaseDate:string,
               public posterPath:string) {
   }
