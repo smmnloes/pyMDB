447bfa57631e1716e1d0bb87a9cf3e3bb08bee8b
maximilian <m.loesch@hotmail.de>
2018-05-12 13:04:56 +0200

    remove api key

--- angular-frontend/src/app/header/content/details-page/details-page.component.ts
+++ angular-frontend/src/app/header/content/details-page/details-page.component.ts
@@ -4,7 +4,7 @@ import {HttpClient} from "@angular/common/http";
 import {Subject} from "rxjs/Subject";
 import {ActivatedRoute} from "@angular/router";
 import {DetailedDataModel} from "../search-page/search-results/result/detailed-data-model";
-import {TMDB_API_KEY} from "../../../util/tmdb-api-key";
+import {TMDB_API_KEY} from "../../../tmdb-api-key";
 
 @Component({
   selector: 'app-details-page',
