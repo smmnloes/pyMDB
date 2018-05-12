1eddd51e9d7e2450c30ebe058266df942fd08fcb
maximilian <m.loesch@hotmail.de>
2018-05-12 13:03:04 +0200

    implement cache for detailed data

--- angular-frontend/src/app/services/detail.service.ts
+++ angular-frontend/src/app/services/detail.service.ts
@@ -7,37 +7,37 @@ import {DetailedDataModel} from "../header/content/search-page/search-results/re
 
 import {Iso639} from "../util/iso639";
 import {TMDB_API_KEY} from "../util/tmdb-api-key";
+import {CacheService} from "./cache.service";
 
 @Injectable()
 export class DetailService {
-
-
   TMDB_ROOT = "https://api.themoviedb.org/3/";
-
-  detailedDataCache: DetailedDataModel;
 
   detailedDataSource: Subject<DetailedDataModel> = new Subject<DetailedDataModel>();
   detailedData$: Observable<DetailedDataModel> = this.detailedDataSource.asObservable();
 
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private cacheService: CacheService) {
   }
 
-  getDetails(tid: number) {
+  getDetails(IMDB_Id: number) {
+    let cachedDetails = this.cacheService.getDetails(IMDB_Id);
+    if (cachedDetails != null) {
+      this.detailedDataSource.next(cachedDetails);
+      return;
+    }
 
-    let imdbIdFormatted = DetailService.getTidFormatted(tid);
+    let IMDB_Id_Formatted = DetailService.formatIMDB_Id(IMDB_Id);
 
-    this.getTmdbID(imdbIdFormatted).subscribe(tmdbID => {
-
-      if (tmdbID != -1) {
-        this.getDetailsAndCastByTmdbId(tmdbID).subscribe(details => {
-          let detailedData = this.processDetailedData(details);
-          this.detailedDataCache = detailedData;
-          this.detailedDataSource.next(detailedData);
-        });
-      } else {
-        this.detailedDataCache = null;
+    this.getTMDB_Id(IMDB_Id_Formatted).subscribe(tmdbID => {
+      if (tmdbID == -1) {
         this.detailedDataSource.next(null);
+      } else {
+        this.getDetailsForTMDB_Id(tmdbID).subscribe(detailedData => {
+          let detailedDataProcessed = this.processDetailedData(detailedData);
+          this.detailedDataSource.next(detailedDataProcessed);
+          this.cacheService.setDetails(IMDB_Id, detailedDataProcessed);
+        });
       }
 
     });
@@ -63,7 +63,7 @@ export class DetailService {
     return creditsProcessed;
   }
 
-  private static getTidFormatted(tidAsInt: number) {
+  private static formatIMDB_Id(tidAsInt: number) {
     let tidAsString = tidAsInt.toString();
     let numberLeadingZeroes = 7 - tidAsString.length;
     let leadingZeroes = "";
@@ -74,7 +74,7 @@ export class DetailService {
 
   }
 
-  private getTmdbID(tid: string) {
+  private getTMDB_Id(tid: string) {
     return this.http.get(
       this.TMDB_ROOT
       + 'find/'
@@ -92,7 +92,7 @@ export class DetailService {
       });
   }
 
-  private getDetailsAndCastByTmdbId(tmdbID: number) {
+  private getDetailsForTMDB_Id(tmdbID: number) {
     return this.http.get(
       this.TMDB_ROOT
       + 'movie/'
@@ -100,9 +100,5 @@ export class DetailService {
       + "?api_key="
       + TMDB_API_KEY
       + "&append_to_response=credits")
-  }
-
-  getCachedDetails() {
-    this.detailedDataSource.next(this.detailedDataCache);
   }
 }
