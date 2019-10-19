152d493acc02555f79452899d932654758f51284
maximilian <m.loesch@hotmail.de>
2019-10-19 19:55:16 +0200

    dockerize

--- py-backend/src/definitions.py
+++ py-backend/src/definitions.py
@@ -2,8 +2,8 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DB_LAST_VERSION = ROOT_DIR + "/../../DB_Data_Local/last_version/local.db"
-DB_DATA_REMOTE = ROOT_DIR + "/../../DB_Data_Remote/"
-LOCAL_DB = ROOT_DIR + "/../../DB_Data_Local/local.db"
+DB_LAST_VERSION = ROOT_DIR + "/../DB_Data_Local/last_version/local.db"
+DB_DATA_REMOTE = ROOT_DIR + "/../DB_Data_Remote/"
+LOCAL_DB = ROOT_DIR + "/../DB_Data_Local/local.db"
 
 URL_IMDB_DATA = "https://datasets.imdbws.com/"
