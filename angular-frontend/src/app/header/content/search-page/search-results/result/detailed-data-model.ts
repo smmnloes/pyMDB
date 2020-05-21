84bd0c184220a05a0aa38f5fdf960b79863a4152
Maximilian Lösch <m.loesch@hotmail.de>
2020-05-21 12:55:41 +0200

    format date in en-US locale

--- angular-frontend/src/app/header/content/search-page/search-results/result/detailed-data-model.ts
+++ angular-frontend/src/app/header/content/search-page/search-results/result/detailed-data-model.ts
@@ -1,10 +1,12 @@
+import {Iso639} from "../../../../../util/iso639";
+
 export class DetailedDataModel {
 
   constructor(public credits: string[][],
               public budget: number,
               public originalLanguage: string,
               public productionCountries: string[],
-              public releaseDate: Date,
+              public releaseDate: string,
               public posterPath: string,
               public overview: string,
               public hasDetails: boolean) {
@@ -18,4 +20,31 @@ export class DetailedDataModel {
     return new DetailedDataModel(null, null, null, null, null,
       null, null, false)
   }
+
+  static fromJSON(json: Object) {
+    return new DetailedDataModel(
+      DetailedDataModel.processCredits(json['credits']),
+      json['budget'],
+      Iso639.iso639ToName[json['original_language']],
+      json['production_countries'].map(element => element['name']),
+      formatReleaseDate(json['release_date']),
+      json['poster_path'],
+      json['overview'],
+      true);
+  }
+
+  private static processCredits(credits: Object): string[][] {
+    let creditsProcessed: string[][] = [];
+
+    for (let cast of credits['cast']) {
+      creditsProcessed.push([cast['name'], cast['character']]);
+    }
+    return creditsProcessed;
+  }
+}
+
+function formatReleaseDate(jsonElement: any): string {
+  let date: Date = new Date(jsonElement);
+  let options = {month: '2-digit', day: '2-digit', year: 'numeric'};
+  return date.toLocaleDateString('en', options)
 }
--- angular-frontend/src/app/services/detail.service.ts
+++ angular-frontend/src/app/services/detail.service.ts
@@ -3,14 +3,11 @@ import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable, of, throwError} from "rxjs";
 import "rxjs/add/operator/map";
 import {DetailedDataModel} from "../header/content/search-page/search-results/result/detailed-data-model";
-
-import {Iso639} from "../util/iso639";
 import {CacheService} from "./cache.service";
 import {catchError, map} from "rxjs/operators";
 
 @Injectable()
 export class DetailService {
-  TMDB_ROOT = "https://api.themoviedb.org/3/";
 
   constructor(private http: HttpClient, private cacheService: CacheService) {
   }
@@ -38,7 +35,7 @@ export class DetailService {
     return this.http.get('api/details', options).pipe(
       map(
         result => {
-          let detailedDataProcessed = DetailService.processDetailedData(result);
+          let detailedDataProcessed = DetailedDataModel.fromJSON(result);
           this.cacheDetailData(detailedDataProcessed, IMDB_Id);
           return detailedDataProcessed;
         }
@@ -52,26 +49,5 @@ export class DetailService {
 
   private cacheDetailData(detailedData: DetailedDataModel, IMDB_Id: number): void {
     this.cacheService.setDetails(IMDB_Id, detailedData)
-  }
-
-  private static processDetailedData(details: Object): DetailedDataModel {
-    return new DetailedDataModel(
-      DetailService.processCredits(details['credits']),
-      details['budget'],
-      Iso639.iso639ToName[details['original_language']],
-      details['production_countries'].map(element => element['name']),
-      new Date(details['release_date']),
-      details['poster_path'],
-      details['overview'],
-      true);
-  }
-
-  private static processCredits(credits: Object): string[][] {
-    let creditsProcessed: string[][] = [];
-
-    for (let cast of credits['cast']) {
-      creditsProcessed.push([cast['name'], cast['character']]);
-    }
-    return creditsProcessed;
   }
 }
