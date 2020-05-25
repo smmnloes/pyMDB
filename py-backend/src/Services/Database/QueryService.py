92341ac5a3c33ba28fd12d1d1899c7a4839b8f63
Maximilian Lösch <m.loesch@hotmail.de>
2020-05-25 18:20:35 +0200

    add wildcard front back in

--- py-backend/src/Services/Database/QueryService.py
+++ py-backend/src/Services/Database/QueryService.py
@@ -58,7 +58,7 @@ def get_movies_by_criteria(request, get_count=False):
 
     if request['title']:
         title_normalized = normalize(request['title'])
-        sub_query = db.session.query(Akas.tid).filter(Akas.title_normalized.like(title_normalized + '%'))
+        sub_query = db.session.query(Akas.tid).filter(Akas.title_normalized.like('%{}%'.format(title_normalized)))
         query = query.filter(Basics.tid.in_(sub_query))
 
     if request['director']:
