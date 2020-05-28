b0d1383a3c42311c3d76c4ecd71ec551056884ee
Maximilian Lösch <m.loesch@hotmail.de>
2020-05-28 11:31:20 +0200

    remove command line interface

--- py-backend/src/main.py
+++ py-backend/src/main.py
@@ -14,30 +14,6 @@ def main(argv):
     app_process = Process(target=AppMain.start_app)
     app_process.start()
 
-    user_input = ''
-    while user_input != 'exit':
-        user_input = input('Please enter Command! (\'exit\' to quit)')
-
-        if user_input == 'update':
-            try:
-                UpdateService.update_db()
-            except (Exception, BaseException):
-                break
-
-        elif user_input == 'download':
-            next_input = input('Download which dataset? ')
-            UpdateService.download_and_unzip_new_data(next_input)
-        elif user_input == 'backup':
-            UpdateService.backup_local_db()
-        elif user_input == 'restore':
-            UpdateService.restore_db_last_version()
-        elif user_input == 'read':
-            next_input = input('Read which dataset? ')
-            UpdateService.DATASETS_TO_READ_FUNCTIONS.get(next_input)()
-        else:
-            print("Unknown command '{}'".format(user_input))
-    shutdown(app_process)
-
 
 def parse_arguments(argv):
     try:
@@ -47,14 +23,10 @@ def parse_arguments(argv):
 
     for arg in args:
         if arg == "update":
+            print("Updating database...")
             AppMain.create_app()
             UpdateService.update_db()
             exit(0)
-
-
-def shutdown(process):
-    process.terminate()
-    process.join()
 
 
 if __name__ == '__main__':
