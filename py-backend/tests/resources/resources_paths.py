ab4189dc753fc89733f649f0dbbac8c7be9a0735
Maximilian Lösch <m.loesch@hotmail.de>
2020-06-02 14:07:11 +0200

    change test pathes, change assertEquals to assertEqual

--- py-backend/tests/resources/resources_paths.py
+++ py-backend/tests/resources/resources_paths.py
@@ -1,5 +1,7 @@
 import os
 
 RESOURCES_ROOT = os.path.dirname(os.path.abspath(__file__))
-TEST_DB_PATH = os.path.join(RESOURCES_ROOT, 'test_db', 'test.db')
+TEST_DBS_PATH = os.path.join(RESOURCES_ROOT, 'test_dbs')
+TEST_TEMP_DB_PATH = os.path.join(TEST_DBS_PATH, 'temp.db')
+TEST_QUERY_DB_PATH = os.path.join(TEST_DBS_PATH, 'query.db')
 TEST_TEMP_PATH = os.path.join(RESOURCES_ROOT, 'temp')
--- py-backend/tests/update_service_test.py
+++ py-backend/tests/update_service_test.py
@@ -5,11 +5,11 @@ from unittest.mock import patch, MagicMock
 from flask import Flask
 
 from model.database_model import *
-from resources.resources_paths import TEST_DB_PATH, TEST_TEMP_PATH
+from resources.resources_paths import TEST_TEMP_PATH, TEST_TEMP_DB_PATH
 from services.database import update_service
 
 
-@patch('services.config.config_service.get_movie_db_path', MagicMock(return_value=TEST_DB_PATH))
+@patch('services.config.config_service.get_movie_db_path', MagicMock(return_value=TEST_TEMP_DB_PATH))
 @patch('services.config.config_service.get_temp_path', MagicMock(return_value=TEST_TEMP_PATH))
 class TestUpdateService(unittest.TestCase):
     test_app = None
@@ -22,21 +22,21 @@ class TestUpdateService(unittest.TestCase):
 
     @classmethod
     def setUp(cls):
-        if os.path.exists(TEST_DB_PATH):
-            os.remove(TEST_DB_PATH)
+        if os.path.exists(TEST_TEMP_DB_PATH):
+            os.remove(TEST_TEMP_DB_PATH)
         with cls.test_app.app_context():
             db.create_all()
 
     def test_read_basics(self):
         update_service.read_basics()
         with self.test_app.app_context():
-            self.assertEquals(db.session.query(Basics).count(), 4)
-            self.assertEquals(db.session.query(Basics).filter(Basics.primaryTitle == 'Short film').count(), 0)
-            self.assertEquals(db.session.query(Basics).filter(Basics.primaryTitle == 'Adult movie').count(), 0)
+            self.assertEqual(db.session.query(Basics).count(), 4)
+            self.assertEqual(db.session.query(Basics).filter(Basics.primaryTitle == 'Short film').count(), 0)
+            self.assertEqual(db.session.query(Basics).filter(Basics.primaryTitle == 'Adult movie').count(), 0)
 
 
 def create_test_app():
     test_app = Flask(__name__)
     test_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    test_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + TEST_DB_PATH
+    test_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + TEST_TEMP_DB_PATH
     return test_app
