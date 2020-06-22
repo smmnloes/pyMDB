0fc59c2ea300583fcc821e294712a23babed66fd
Maximilian Lösch <m.loesch@hotmail.de>
2020-06-22 12:14:55 +0200

    refactor app creation

--- py-backend/main/app/app_main.py
+++ py-backend/main/app/app_main.py
@@ -25,9 +25,51 @@ def start_app():
 
 
 def create_app():
+    new_app = Flask(__name__)
+    init_app_config(new_app)
+    db.init_app(new_app)
+    init_app_logger(new_app)
+    init_app_cache(new_app)
+    init_app_api(new_app)
+    init_app_user_mgmt(new_app)
     global pymdb_app
-    pymdb_app = Flask(__name__)
-    pymdb_app.config.update(
+    pymdb_app = new_app
+    return new_app
+
+
+def init_app_user_mgmt(app):
+    from model.user_model import User
+    UserManager(app, db, User)
+    if not os.path.exists(config_service.get_user_db_path()):
+        with app.app_context():
+            db.create_all(bind=BIND_USERS)
+
+
+def init_app_api(app):
+    api = Api(app, errors=rest_api_errors)
+    CORS(app, resources={"/query": {'methods': ['POST']}})
+    from api.rest_controllers import MovieQuery, ResultCount, MovieByTid, TmdbDetailedData, HasDetails
+    api.add_resource(MovieQuery, urljoin(API_ROOT, 'query'))
+    api.add_resource(ResultCount, urljoin(API_ROOT, 'result_count'))
+    api.add_resource(MovieByTid, urljoin(API_ROOT, 'movie_by_tid'))
+    api.add_resource(TmdbDetailedData, urljoin(API_ROOT, 'details'))
+    api.add_resource(HasDetails, urljoin(API_ROOT, 'has_details'))
+
+
+def init_app_cache(app):
+    global cache
+    cache = Cache(app, config={'CACHE_TYPE': 'simple',
+                               'CACHE_DEFAULT_TIMEOUT': 0})
+
+
+def init_app_logger(app):
+    global logger
+    logger = app.logger
+    logger.setLevel(logging.INFO)
+
+
+def init_app_config(app):
+    app.config.update(
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
         SQLALCHEMY_BINDS={
             BIND_USERS: 'sqlite:///' + config_service.get_user_db_path(),
@@ -36,33 +78,3 @@ def create_app():
         USER_EMAIL_SENDER_EMAIL='max@pymdb.mloesch.it',
         SECRET_KEY=config_service.get_app_key()
     )
-    db.init_app(pymdb_app)
-
-    global logger
-    logger = pymdb_app.logger
-    logger.setLevel(logging.INFO)
-
-    global cache
-    cache = Cache(pymdb_app, config={'CACHE_TYPE': 'simple',
-                                     'CACHE_DEFAULT_TIMEOUT': 0})
-
-    api = Api(pymdb_app, errors=rest_api_errors)
-    CORS(pymdb_app, resources={"/query": {'methods': ['POST']}})
-    from api.rest_controllers import MovieQuery, ResultCount, MovieByTid, TmdbDetailedData, HasDetails
-    api.add_resource(MovieQuery, urljoin(API_ROOT, 'query'))
-    api.add_resource(ResultCount, urljoin(API_ROOT, 'result_count'))
-    api.add_resource(MovieByTid, urljoin(API_ROOT, 'movie_by_tid'))
-    api.add_resource(TmdbDetailedData, urljoin(API_ROOT, 'details'))
-    api.add_resource(HasDetails, urljoin(API_ROOT, 'has_details'))
-
-    from model.user_model import User
-    UserManager(pymdb_app, db, User)
-    create_user_db()
-
-    return pymdb_app
-
-
-def create_user_db():
-    if not os.path.exists(config_service.get_user_db_path()):
-        with pymdb_app.app_context():
-            db.create_all(bind=BIND_USERS)
